a7288a9a359268667fff9f4214a1488715b084b7
ParagMadhwai <98086301+ParagMadhwai@users.noreply.github.com>
2022-01-29 08:16:52 +0530

    Update server.js

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
  app.get('/', function (req, res) {
   /* res.sendFile(path.join(___dirname + '/candidateInfo.html'));*/
-   res.sendFile(__dirname + '/candidateInfo.html');
+   res.sendFile(__dirname + '/public/candidateInfo.html');
 });
  
 
